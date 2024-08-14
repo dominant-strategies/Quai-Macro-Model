32e64e8c273c4d6f91ac8207f0bdfddffc402d15
SeanMcOwen <seanmcowen@gmail.com>
2024-08-14 13:34:54 -0400

    Start implementation

--- Spec/src/Implementations/Python/Metrics/Reward.py
+++ Spec/src/Implementations/Python/Metrics/Reward.py
@@ -1,14 +1,19 @@
+from math import log
+
+
 def qi_to_hash_metric(state, params, spaces):
-    pass
+    return spaces[0]["Qi"] * state["K Qi"]
 
 
 def quai_to_hash_metric(state, params, spaces):
-    pass
+    return params["Quai Reward Base Parameter"] ** (spaces[0]["Quai"] / state["K Quai"])
 
 
 def hash_to_qi_metric(state, params, spaces):
-    pass
+    return spaces[0]["Hash"] / state["K Qi"]
 
 
 def hash_to_quai_metric(state, params, spaces):
-    pass
+    return state["K Quai"] * log(
+        spaces[0]["Hash"], params["Quai Reward Base Parameter"]
+    )
