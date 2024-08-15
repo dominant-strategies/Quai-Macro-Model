549cd5e5f6bb14c1e764b6a9232fcd63d08c91b6
SeanMcOwen <seanmcowen@gmail.com>
2024-08-15 15:52:16 -0400

    Finish mechanisms

--- Spec/src/Implementations/Python/Mechanisms/MonetaryPolicy.py
+++ Spec/src/Implementations/Python/Mechanisms/MonetaryPolicy.py
@@ -1,14 +1,30 @@
 def mint_qi_tokens_mechanism(state, params, spaces):
-    pass
+    amount = spaces[0]["Qi"]
+    if amount == 0:
+        return
+    assert amount > 0, "Amount must be positive"
+    state["Qi Supply"] += amount
 
 
 def mint_quai_token_mechanism(state, params, spaces):
-    pass
+    amount = spaces[0]["Quai"]
+    if amount == 0:
+        return
+    assert amount > 0, "Amount must be positive"
+    state["Quai Supply"] += amount
 
 
 def burn_qi_tokens_mechanism(state, params, spaces):
-    pass
+    amount = spaces[0]["Qi"]
+    if amount == 0:
+        return
+    assert amount > 0, "Amount must be positive"
+    state["Qi Supply"] -= amount
 
 
 def burn_quai_tokens_mechanism(state, params, spaces):
-    pass
+    amount = spaces[0]["Quai"]
+    if amount == 0:
+        return
+    assert amount > 0, "Amount must be positive"
+    state["Quai Supply"] -= amount
