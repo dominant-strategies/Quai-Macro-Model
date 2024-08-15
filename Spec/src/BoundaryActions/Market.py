af3ca3c2de8f1abf47602d362ee1142bf656add4
SeanMcOwen <seanmcowen@gmail.com>
2024-08-15 12:34:09 -0400

    Test functions

--- Spec/src/BoundaryActions/Market.py
+++ Spec/src/BoundaryActions/Market.py
@@ -1,8 +1,20 @@
+test_quai_conversion = {
+    "name": "TEST Quai Conversion",
+    "description": "Test function that moves to exchange 100 Quai.",
+    "logic": "Return a space with quai conversion of 100",
+}
+
+test_qi_conversion = {
+    "name": "TEST Qi Conversion",
+    "description": "Test function that moves to exchange 100 Qi.",
+    "logic": "Return a space with qi conversion of 100",
+}
+
 conversions_boundary_action = {
     "name": "Conversions Boundary Action",
     "description": "Boundary action which determines amount of potentially converted Qi or Quai.",
     "constraints": [],
-    "boundary_action_options": [],
+    "boundary_action_options": [test_quai_conversion, test_qi_conversion],
     "called_by": [],
     "codomain": ["Conversion Space"],
     "parameters_used": [],
