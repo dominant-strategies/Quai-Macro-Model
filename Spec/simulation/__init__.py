fb6f19a9289f3ecf3bd80f3a7413d571cd394c57
SeanMcOwen <seanmcowen@gmail.com>
2024-08-15 12:46:38 -0400

    Issue 173

---
+++ Spec/simulation/__init__.py
@@ -0,0 +1,1 @@
+from .config import state_base, params_base
---
+++ Spec/simulation/config/params.py
@@ -0,0 +1,12 @@
+params_base = {
+    "Minimum Quai Conversion Amount": None,
+    "Minimum Qi Conversion Amount": None,
+    "Asset Return Parameterization": None,
+    "PID Parameterization": None,
+    "Initial Block Difficulty": None,
+    "Block Difficulty Multiples": {"Prime": 1, "Region": 0.5, "Zone": 0.25},
+    "Target Mining Time": 2,
+    "Quai Reward Base Parameter": 2,
+    "FP Controller Update Policy": "Linear Controller Policy",
+    "FP Conversions Boundary Action": "TEST Quai Conversion",
+}
