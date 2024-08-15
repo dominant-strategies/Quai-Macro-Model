b1ea5768176c141016bb45b5683f8baaf3e4e0c9
SeanMcOwen <seanmcowen@gmail.com>
2024-08-15 15:13:09 -0400

    Mechs

---
+++ Spec/src/Implementations/Python/Mechanisms/Logs.py
@@ -0,0 +1,6 @@
+def update_historical_converted_qi_mechanism(state, params, spaces):
+    pass
+
+
+def update_historical_converted_quai_mechanism(state, params, spaces):
+    pass
---
+++ Spec/src/Implementations/Python/Mechanisms/MonetaryPolicy.py
@@ -0,0 +1,14 @@
+def mint_qi_tokens_mechanism(state, params, spaces):
+    pass
+
+
+def mint_quai_token_mechanism(state, params, spaces):
+    pass
+
+
+def burn_qi_tokens_mechanism(state, params, spaces):
+    pass
+
+
+def burn_quai_tokens_mechanism(state, params, spaces):
+    pass
