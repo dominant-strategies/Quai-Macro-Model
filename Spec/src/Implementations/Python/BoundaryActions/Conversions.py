e73d2bde0064e8b4409c40b5f39952730429f6b2
SeanMcOwen <seanmcowen@gmail.com>
2024-08-15 13:19:48 -0400

    Scaffold

---
+++ Spec/src/Implementations/Python/BoundaryActions/Conversions.py
@@ -0,0 +1,6 @@
+def test_quai_conversion(state, params, spaces):
+    pass
+
+
+def test_qi_conversion(state, params, spaces):
+    pass
--- Spec/src/Implementations/Python/BoundaryActions/__init__.py
+++ Spec/src/Implementations/Python/BoundaryActions/__init__.py
@@ -1,9 +1,12 @@
 from .Dummy import v1_dummy_boundary, v1_dummy_boundary2, v2_dummy_boundary2
 from .Block import mine_block_boundary_action_v1
+from .Conversions import test_quai_conversion, test_qi_conversion
 
 boundary_action_options = {
     "V1 Dummy Boundary Action Option": v1_dummy_boundary,
     "V1 Dummy Boundary Action 2 Option": v1_dummy_boundary2,
     "V2 Dummy Boundary Action 2 Option": v2_dummy_boundary2,
     "Mine Block Boundary Action V1": mine_block_boundary_action_v1,
+    "TEST Quai Conversion": test_quai_conversion,
+    "TEST Qi Conversion": test_qi_conversion,
 }
