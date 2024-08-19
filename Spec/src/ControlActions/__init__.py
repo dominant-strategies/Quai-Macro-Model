4e20cf20708d42ef6ba9f1b0571413e6a038056a
SeanMcOwen <seanmcowen@gmail.com>
2024-08-19 16:59:10 -0400

    Control action

--- Spec/src/ControlActions/__init__.py
+++ Spec/src/ControlActions/__init__.py
@@ -1,7 +1,9 @@
 from .Simulation import simulation_control_actions
 from .Controller import controller_control_actions
+from .Vesting import vesting_control_actions
 
 control_actions = []
 
 control_actions.extend(simulation_control_actions)
 control_actions.extend(controller_control_actions)
+control_actions.extend(vesting_control_actions)
