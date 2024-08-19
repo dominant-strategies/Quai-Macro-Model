7405918d73b9e6ecfbc160d7213c31c4ac766a2a
SeanMcOwen <seanmcowen@gmail.com>
2024-08-19 17:31:43 -0400

    Control actio implementation

--- Spec/src/ControlActions/Vesting.py
+++ Spec/src/ControlActions/Vesting.py
@@ -1,8 +1,14 @@
+unlock_tokens_control_action_option1 = {
+    "name": "Unlock Tokens Control Action V1",
+    "description": "Simple pass through to start chain of actions",
+    "logic": "",
+}
+
 unlock_tokens_control_action = {
     "name": "Unlock Tokens Control Action",
     "description": "Control action that triggers unlocking of tokens. No space is passed out since with this implementation it just meant to start the policy.",
     "constraints": [],
-    "control_action_options": [],
+    "control_action_options": [unlock_tokens_control_action_option1],
     "codomain": [],
     "parameters_used": [],
 }
