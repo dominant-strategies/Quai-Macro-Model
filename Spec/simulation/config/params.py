c8a2e4e86bc6410ff17238ad2a4e56c78404fe87
SeanMcOwen <seanmcowen@gmail.com>
2024-08-19 10:01:43 -0400

    Begin writing intial vesting schedule

--- Spec/simulation/config/params.py
+++ Spec/simulation/config/params.py
@@ -7,6 +7,15 @@ params_base = {
     "Block Difficulty Multiples": {"Prime": 1, "Region": 0.5, "Zone": 0.25},
     "Target Mining Time": 2,
     "Quai Reward Base Parameter": 2,
+    "Initial Vesting Schedule": [
+        {
+            "vesting_amount": 1000000000 * 0.3,
+            "vesting_frequency": "Immediate",
+            "time": 0,
+            "recipient": "Foundation",
+            "duration": None,
+        }
+    ],
     "FP Controller Update Policy": "Linear Controller Policy",
     "FP Conversions Boundary Action": "TEST Quai Conversion",
 }
--- Spec/src/Types/Vesting.py
+++ Spec/src/Types/Vesting.py
@@ -1,7 +1,7 @@
 VestingScheduleType = {
     "name": "Vesting Schedule Type",
     "type": "VestingScheduleType",
-    "notes": "A list of objects with the following attributes: vesting_amount, vesting_frequency, recipient, and optionally duration if the vesting_frequency is not immediate",
+    "notes": "A list of objects with the following attributes: vesting_amount, vesting_frequency, recipient, time (in years) for when it is active, and optionally duration if the vesting_frequency is not immediate",
 }
 
 vesting_types = [VestingScheduleType]
