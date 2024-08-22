53debfffa70e1c66f5be664efe5d0be4dd460df1
SeanMcOwen <seanmcowen@gmail.com>
2024-08-22 18:22:51 -0400

    Finish current lockup options

--- Spec/src/Implementations/Python/StatefulMetrics/Vesting.py
+++ Spec/src/Implementations/Python/StatefulMetrics/Vesting.py
@@ -4,3 +4,10 @@ def circulating_quai_supply(state, params):
 
 def circulating_qi_supply(state, params):
     return state["Qi Supply"] - state["Locked Qi Supply"]
+
+
+def current_lockup_options(state, params):
+    year = 1 + int(state["Time"] / 365)
+    year = min(year, max(params["Lockup Options"].keys()))
+    print(year)
+    return params["Lockup Options"][year]
