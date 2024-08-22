a3b5c3a71d3c5d03aecb1a1651e17cd8fb7270c1
SeanMcOwen <seanmcowen@gmail.com>
2024-08-22 15:48:23 -0400

    Add implementation

--- Spec/src/BoundaryActions/Market.py
+++ Spec/src/BoundaryActions/Market.py
@@ -48,8 +48,8 @@ $$P_{Quai}^{t+1} = \lambda C_i(...)P_{Qi} + (1-\lambda)P_{Quai}^{t} + NORM(0, \s
 
 Conversion into a return space (for compatibility with the way the block logic works/space output schema):
 
-$$r_{Qi} = 1 - \frac{P_{Qi}^{t+1}}{P_{Qi}^{t}}$$
-$$r_{Quai} = 1 - \frac{P_{Quai}^{t+1}}{P_{Quai}^{t}}$$""",
+$$r_{Qi} = \frac{P_{Qi}^{t+1}}{P_{Qi}^{t}} - 1$$
+$$r_{Quai} = \frac{P_{Quai}^{t+1}}{P_{Quai}^{t}} - 1$$""",
 }
 
 price_movements_boundary_action = {
--- Spec/src/Implementations/Python/BoundaryActions/Market.py
+++ Spec/src/Implementations/Python/BoundaryActions/Market.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def test_price_movements_boundary(state, params, spaces):
     space = {"Qi Return": 0.05, "Quai Return": 0.1}
     return [space]
@@ -5,4 +8,23 @@ def test_price_movements_boundary(state, params, spaces):
 
 def hashpower_price_movement(state, params, spaces):
     ewm_lambda = params["Price EWMA Lambda"]
-    c = state["Metrics"]["Conversion Rate Metric"]()
+    c = state["Metrics"]["Conversion Rate Metric"](state, params, [])
+    p_quai = state["Quai Price"]
+    p_qi = state["Qi Price"]
+    hashpower_cost = params["Hashpower Cost Series"][state["Block Number"]]
+    qi_sigma = params["Qi Price Movemement Sigma"]
+    quai_sigma = params["Quai Price Movemement Sigma"]
+
+    p_qi_new = (
+        ewm_lambda * hashpower_cost
+        + (1 - ewm_lambda) * p_qi
+        + np.random.normal(0, qi_sigma)
+    )
+    p_quai_new = (
+        ewm_lambda * c * p_qi
+        + (1 - ewm_lambda) * p_quai
+        + np.random.normal(0, quai_sigma)
+    )
+
+    space = {"Qi Return": p_qi_new / p_qi - 1, "Quai Return": p_quai_new / p_quai - 1}
+    return [space]
