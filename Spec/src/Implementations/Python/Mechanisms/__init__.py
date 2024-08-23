25789abad251f2c7814e41c224a805d443d9058b
SeanMcOwen <seanmcowen@gmail.com>
2024-08-23 08:54:28 -0400

    Finish mechs

--- Spec/src/Implementations/Python/Mechanisms/__init__.py
+++ Spec/src/Implementations/Python/Mechanisms/__init__.py
@@ -13,7 +13,10 @@ from .Logs import (
     update_historical_converted_quai_mechanism,
 )
 from .Vesting import unlock_tokens_mechanism
-from .Block import update_block_difficulty_mechanism
+from .Block import (
+    update_block_difficulty_mechanism,
+    append_to_unlock_schedule_mechanism,
+)
 
 mechanisms = {
     "DUMMY Log Results Mechanism": dummy_log_results_mechanism,
@@ -28,4 +31,5 @@ mechanisms = {
     "Update Block Difficulty Mechanism": update_block_difficulty_mechanism,
     "Update Locked Qi Mechanism": update_locked_qi_mechanism,
     "Update Locked Quai Mechanism": update_locked_quai_mechanism,
+    "Append to Unlock Schedule Mechanism": append_to_unlock_schedule_mechanism,
 }
