27a6e73b0e29b87d9d22ba5f5d3ead60dcd93a81
SeanMcOwen <seanmcowen@gmail.com>
2024-08-23 10:18:13 -0400

    Checkpoint

--- Spec/src/Implementations/Python/BoundaryActions/Conversions.py
+++ Spec/src/Implementations/Python/BoundaryActions/Conversions.py
@@ -1,5 +1,23 @@
+from random import random
+
+
 def conversions_boundary_action_v1(state, params, spaces):
-    return test_quai_conversion(state, params, spaces)
+    if len(state["Historical Mined Ratio"]) > 0:
+        quai_probability = state["Historical Mined Ratio"][-1]
+    else:
+        quai_probability = 0.5
+    if random() < quai_probability:
+        q = "Quai"
+    else:
+        q = "Qi"
+    if q == "Quai":
+        circulating = state["Stateful Metrics"]["Circulating Quai Supply"](
+            state, params
+        )
+    else:
+        circulating = state["Stateful Metrics"]["Circulating Qi Supply"](state, params)
+    print(circulating)
+    assert False
 
 
 def test_quai_conversion(state, params, spaces):
