489488b56115642ef226815dcf09477d5dcf5a52
SeanMcOwen <seanmcowen@gmail.com>
2024-08-26 19:27:15 -0400

    Update spaces

--- Spec/src/Spaces/Block.py
+++ Spec/src/Spaces/Block.py
@@ -56,7 +56,16 @@ mined_blocks_space = {
 
 mined_blocks_space2 = {
     "name": "Mined Blocks Space 2",
-    "schema": {},
+    "schema": {
+        "Quai Reward Offered": "Quai Array Type",
+        "Qi Reward Offered": "Qi Array Type",
+        "Block Difficulty": "Hash Array Type",
+        "Mining Time": "Delta Time Type",
+        "Quai Taken": "Quai Array Type",
+        "Qi Taken": "Qi Array Type",
+        "New Difficulty": "Block Difficulty Type",
+        "Mining Time": "Delta Time Type",
+    },
 }
 
 
