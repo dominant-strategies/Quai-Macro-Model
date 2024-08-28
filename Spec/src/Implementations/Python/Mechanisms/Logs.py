6625bf9841679ee4ea86686357c2369572dbc002
SeanMcOwen <seanmcowen@gmail.com>
2024-08-28 14:28:59 -0400

    Scaffold

--- Spec/src/Implementations/Python/Mechanisms/Logs.py
+++ Spec/src/Implementations/Python/Mechanisms/Logs.py
@@ -6,3 +6,8 @@ def update_historical_converted_qi_mechanism(state, params, spaces):
 def update_historical_converted_quai_mechanism(state, params, spaces):
     if spaces[0]:
         state["Historical Converted Quai"].append(spaces[0])
+
+
+def log_simulation_data_mechanism(state, params, spaces):
+    log = {}
+    state["Simulation History Log"].append(log)
