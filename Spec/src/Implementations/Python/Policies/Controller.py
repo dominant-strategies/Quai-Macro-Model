e3a6923fa66d85b64b8f3f4deaa92978ad82fc3b
SeanMcOwen <seanmcowen@gmail.com>
2024-08-28 16:02:19 -0400

    Finish for now

--- Spec/src/Implementations/Python/Policies/Controller.py
+++ Spec/src/Implementations/Python/Policies/Controller.py
@@ -8,6 +8,9 @@ def sgd_logistic_classifier_training(state, params, spaces):
     ]
     Y = [x > 0 for x in spaces[0]["Qi Taken"]]
 
+    # if state["Block Number"] < 20:
+    #    print(X, Y)
+
     state["Logistic Classifier"].partial_fit(X, Y, classes=[0, 1])
     betas = state["Logistic Classifier"].coef_[0]
     return [spaces[0], {"Beta": betas}]
