820af6685c571f66171bcd8f0b0e82e1bb841993
SeanMcOwen <seanmcowen@gmail.com>
2024-08-28 15:57:01 -0400

    Switch out intercept

--- Spec/simulation/preprocessing/controller.py
+++ Spec/simulation/preprocessing/controller.py
@@ -2,5 +2,7 @@ from sklearn.linear_model import SGDClassifier
 
 
 def build_logistic_classifier(state, params):
-    state["Logistic Classifier"] = SGDClassifier(loss="log_loss", penalty=None)
+    state["Logistic Classifier"] = SGDClassifier(
+        loss="log_loss", penalty=None, fit_intercept=False
+    )
     return state
