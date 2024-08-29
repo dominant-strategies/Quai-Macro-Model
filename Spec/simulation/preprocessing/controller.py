756a5e51bdfac594423da965b94a8dd0da24f000
SeanMcOwen <seanmcowen@gmail.com>
2024-08-28 22:58:40 -0400

    Finish up for now

--- Spec/simulation/preprocessing/controller.py
+++ Spec/simulation/preprocessing/controller.py
@@ -1,8 +1,8 @@
 from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 def build_logistic_classifier(state, params):
-    state["Logistic Classifier"] = SGDClassifier(
-        loss="log_loss", penalty=None, fit_intercept=False
-    )
+
+    SGDClassifier(loss="log_loss", penalty=None, fit_intercept=False)
     return state
