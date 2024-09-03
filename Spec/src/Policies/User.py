5b374057cb27ffb18b42f836461ea1c261e04d16
SeanMcOwen <seanmcowen@gmail.com>
2024-09-03 09:08:40 -0400

    Add policy

---
+++ Spec/src/Policies/User.py
@@ -0,0 +1,13 @@
+user_population_beta_policy = {
+    "name": "Update Population Beta Policy",
+    "description": "Policy for making any revisions or changes to population beta.",
+    "constraints": ["Beta0 < 0 and Beta1 > 0"],
+    "policy_options": [],
+    "domain": ["Beta Vector Space"],
+    "codomain": ["Beta Vector Space"],
+    "parameters_used": [],
+    "metrics_used": [],
+}
+
+
+user_policies = [user_population_beta_policy]
