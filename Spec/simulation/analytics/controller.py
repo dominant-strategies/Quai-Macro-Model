282dc493bbc6f04bac4bdc0828e0fc2c8f03e0f5
SeanMcOwen <seanmcowen@gmail.com>
2024-09-10 14:13:17 -0400

    Issue 312

--- Spec/simulation/analytics/controller.py
+++ Spec/simulation/analytics/controller.py
@@ -6,6 +6,7 @@ def plot_betas(df):
     df.set_index("Block Number")[["Population Beta0", "Estimate Beta0"]].plot(
         kind="line"
     )
+    plt.xlabel("Prime Block Number")
     plt.ylabel("Beta Value")
     plt.title("Population vs. Estimate of Beta0")
     plt.show()
@@ -13,6 +14,7 @@ def plot_betas(df):
     df.set_index("Block Number")[["Population Beta1", "Estimate Beta1"]].plot(
         kind="line"
     )
+    plt.xlabel("Prime Block Number")
     plt.ylabel("Beta Value")
     plt.title("Population vs. Estimate of Beta0")
     plt.show()
@@ -21,7 +23,7 @@ def plot_betas(df):
 def plot_beta_errors(df):
 
     df.set_index("Block Number")[["Beta0 Error", "Beta1 Error"]].plot(style=".")
-
+    plt.xlabel("Prime Block Number")
     plt.ylabel("Error in Beta Value")
     plt.yscale("log")
     plt.title("Error in Beta0 and Beta1")
@@ -30,6 +32,7 @@ def plot_beta_errors(df):
 
 def plot_beta_error_norm(df):
     df.set_index("Block Number")["Beta Estimation Norm"].plot(style=".")
+    plt.xlabel("Prime Block Number")
     plt.ylabel("Error Norm ")
     plt.yscale("log")
     plt.title("Error Norm for Beta Estimation")
