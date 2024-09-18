c0216ada5a7ee5e3ca794a27f228b852d6f319b3
SeanMcOwen <seanmcowen@gmail.com>
2024-09-17 21:39:42 -0400

    Checkpoint

--- Model/psub.py
+++ Model/psub.py
@@ -1,1 +1,15 @@
 psub_blocks = []
+
+
+def p_log(_params, substep, state_history, state) -> dict:
+    print(_params["MSI"])
+
+
+test_block = {
+    "policies": {
+        "test": p_log,
+    },
+    "variables": {},
+}
+
+psub_blocks.append(test_block)
