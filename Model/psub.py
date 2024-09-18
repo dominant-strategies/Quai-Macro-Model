b9723ace1bc5c73da08cb99732865947f582299d
SeanMcOwen <seanmcowen@gmail.com>
2024-09-17 22:11:33 -0400

    Checkpoint

--- Model/psub.py
+++ Model/psub.py
@@ -1,15 +1,26 @@
 psub_blocks = []
 
 
+[
+    "Price Movements Wiring",
+    "Conversions Wiring",
+    "Mine Block Wiring",
+    "Unlock Tokens Wiring",
+    "Update Population Beta Wiring",
+    "Log Simulation Data Mechanism",
+]
+
+
 def p_log(_params, substep, state_history, state) -> dict:
-    print(_params["MSI"])
+    _params["MSI"].components["Log Simulation Data Mechanism"](state, _params, [])
+    return {}
 
 
 test_block = {
     "policies": {
         "test": p_log,
     },
-    "variables": {},
+    "variables": {"IGNORE": lambda a, b, c, d, e: ("IGNORE", 0)},
 }
 
 psub_blocks.append(test_block)
